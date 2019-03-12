4c83270ff2ce9a11da29437190028b13d80e51f2
wells <wells1013@163.com>
2019-03-12 11:13:21 +0800

    toc

--- src/lib/mixins/markdown.js
+++ src/lib/mixins/markdown.js
@@ -33,7 +33,8 @@ var taskLists = require('markdown-it-task-lists')
 // container
 var container = require('markdown-it-container')
 //
-var toc = require('markdown-it-toc')
+// var toc = require('markdown-it-toc')
+var toc = require('markdown-it-toc-and-anchor').default
 // add target="_blank" to all link
 var defaultRender = markdown.renderer.rules.link_open || function(tokens, idx, options, env, self) {
     return self.renderToken(tokens, idx, options);
@@ -86,7 +87,7 @@ markdown.use(mihe, hljs_opts)
     .use(miip)
     .use(katex)
     .use(taskLists)
-    .use(toc)
+    .use(toc, {'tocFirstLevel': 2, 'anchorLinkSymbol': ''})
 
 export default {
     data() {
